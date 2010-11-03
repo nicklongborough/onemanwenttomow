6650be9e17b0eaac4ab77268714f2f678ce42987
2141 <u58500155@false.(none)>
2010-11-03 14:43:10 +0100

    changed DB connection details

--- lib/config.php
+++ lib/config.php
@@ -3,11 +3,11 @@
 // DB connection info
 
 //Local Development
-$db_ip = 'localhost';
-$db_user = 'web';
+$db_ip = 'db2465.oneandone.co.uk';
+$db_user = 'dbo328145735';
 $db_pass = 'w3bus3r';
 
-$db_user_write = 'web';
+$db_user_write = 'db0328145735';
 $db_pass_write = 'w3bus3r';
 
 // Set the cache limiter to ensure that back button always works, but page is reloaded when a link is clicked (beware: also means that user can click back after logging out)
