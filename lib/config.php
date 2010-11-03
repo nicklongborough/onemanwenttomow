d3d586b76ecf45de27fe9a0a0561ecd883406d9f
2141 <u58500155@false.(none)>
2010-11-03 15:39:51 +0100

    changing config

--- lib/config.php
+++ lib/config.php
@@ -16,9 +16,9 @@ $db_pass_write = 'w3bus3r';
 	} else {
 		$db_ip = 'db2465.oneandone.co.uk';
 		$db_user = 'dbo328145735';
-		$db_pass = 'T6svRkoO';
+		$db_pass = 'w3bus3r';
 		$db_user_write = 'dbo328145735';
-		$db_pass_write = 'T6svRkoO';
+		$db_pass_write = 'w3bus3r';
 	}
 // Set the cache limiter to ensure that back button always works, but page is reloaded when a link is clicked (beware: also means that user can click back after logging out)
 session_cache_limiter('must-revalidate');
