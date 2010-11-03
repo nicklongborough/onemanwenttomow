fa49b27f58df5e78bfc61a8e15a5ce8da42efa7d
unknown <nick.a.richmond@.accenture.com>
2010-11-03 14:38:43 +0000

    adding new config

--- lib/config.php
+++ lib/config.php
@@ -7,6 +7,7 @@ $db_user = 'dbo328145735';
 $db_pass = 'w3bus3r';
 $db_user_write = 'db0328145735';
 $db_pass_write = 'w3bus3r';
+
 	if ($_SERVER["SERVER_NAME"] == 'localhost') {
 		$db_ip = 'localhost';
 		$db_user = 'web';
@@ -16,9 +17,10 @@ $db_pass_write = 'w3bus3r';
 	} else {
 		$db_ip = 'db2465.oneandone.co.uk';
 		$db_user = 'dbo328145735';
-		$db_pass = 'T6svRkoO';
+		$db_pass = 'w3bus3r';
+		$db_ip_write = 'db2465.oneandone.co.uk';
 		$db_user_write = 'dbo328145735';
-		$db_pass_write = 'T6svRkoO';
+		$db_pass_write = 'w3bus3r';
 	}
 // Set the cache limiter to ensure that back button always works, but page is reloaded when a link is clicked (beware: also means that user can click back after logging out)
 session_cache_limiter('must-revalidate');
